8589682b1e43b4d468ca26eaa8afbd754c78e512
Jacky <113130157+JagiZheng@users.noreply.github.com>
2024-05-15 23:19:29 -0400

    Components

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -26,14 +26,6 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
-      path: '/Gorilla',
-      name: 'Gorilla',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Gorilla.vue')
-    },
-    {
       path: '/settings',
       name: 'Settings',
       // route level code-splitting
